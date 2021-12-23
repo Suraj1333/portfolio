365f43f8b45eabeacaf70180ae0f48dff72fa034
Sai Venkata Suraj Kandlakunta <saisuraj.k@gmail.com>
2021-12-23 15:18:20 +0530

    first commit

---
+++ darkmode.js
@@ -0,0 +1,37 @@
+
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  
+  document.body.classList.add('darkmode');
+  
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  
+  document.body.classList.remove('darkmode');
+  
+  localStorage.setItem('darkMode', null);
+}
+ 
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+
+darkModeToggle.addEventListener('click', () => {
+
+  darkMode = localStorage.getItem('darkMode'); 
+  
+
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+   
+  } else {  
+    disableDarkMode(); 
+  }
+});
